0187f3975b959b511d41cf92f0c26dbf9975637b
Barnaeva <161695917+Barnaeva@users.noreply.github.com>
2024-10-29 18:00:18 +0400

    Update iterator.py

--- lab2/iterator.py
+++ lab2/iterator.py
@@ -2,11 +2,10 @@ import os
 
 
 class MyIterator:
-    def __init__(self, name_dir):
-            """конструктор"""
+        def __init__(self, name_dir:str):
         data=[]
         for name in os.listdir(name_dir):
-            data=os.path.join(name_dir,name)
+            data.append(os.path.join(name_dir,name))
         limit=len(data)
         self.data=data
         self.limit = limit
@@ -14,12 +13,10 @@ class MyIterator:
 
 
     def __iter__(self):
-        """возвращает экземпляр итератора"""
         return self
 
 
     def __next__(self):
-        """возвращает следующий элемент последовательности"""
         if self.counter < self.limit:
             self.counter += 1
             return self.data[self.counter-1]
