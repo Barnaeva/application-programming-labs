62f3c7d4f74afdfaa0db47cc354fc435c1d87f9d
Barnaeva <161695917+Barnaeva@users.noreply.github.com>
2024-11-05 09:47:10 +0400

    Update task.py

--- lab3/task.py
+++ lab3/task.py
@@ -3,16 +3,16 @@ import cv2
 from numpy import ndarray
 
 
-def rotation(img: ndarray, size: [int, int], angel: float) -> ndarray:
+def rotation(img: ndarray, size: [int, int], angle: float) -> ndarray:
     """
     Rotates the input image by a specified angle and resizes it.
 
     :param img: Input image (ndarray).
     :param size: Desired output size (height, width).
-    :param angel: Rotation angle in degrees.
+    :param angle: Rotation angle in degrees.
     :return: Rotated and resized image (ndarray).
     """
     height, width = size
-    matrix = cv2.getRotationMatrix2D(((height - 1) / 2.0, (width - 1) / 2.0), angel, 1)
+    matrix = cv2.getRotationMatrix2D(((height - 1) / 2.0, (width - 1) / 2.0), angle, 1)
     dst = cv2.warpAffine(img, matrix, (height, width))
     return dst
