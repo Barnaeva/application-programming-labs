33b92f9d2406ab8d0acfa38f1bfbeab1fb5f05cd
Barnaeva <161695917+Barnaeva@users.noreply.github.com>
2024-11-26 11:26:27 +0400

    Update main_window.py

--- laba5/main_window.py
+++ laba5/main_window.py
@@ -5,8 +5,13 @@ from PyQt5.QtGui import QPixmap
 from iterator import MyIterator
 from PyQt5.QtCore import Qt
 
+
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
+
+    def __init__(self)->None:
+        """
+        make window and layout grid
+        """
         super().__init__()
         self.setWindowTitle("Image Viewer")
         self.setGeometry(500, 300, 600, 400)
@@ -44,7 +49,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.image_iterator = None
         self.current_image = None
 
-    def open_folder_dialog(self):
+
+    def open_folder_dialog(self)->None:
+        """
+        get name folder from file dialog
+        :return: None
+        """
         folder_name = QFileDialog.getExistingDirectory(self, "Select Folder")
         if folder_name:
             self.image_iterator = MyIterator(folder_name)
@@ -56,12 +66,23 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.err_text.setText("No images found in the selected folder.")
                 self.err_text.show()
 
-    def display_image(self, image_path):
+
+    def display_image(self, image_path)->None:
+        """
+        show pictures
+        :param image_path: image path
+        :return:None
+        """
         pixmap = QPixmap(image_path)
         scaled_pixmap = pixmap.scaled(self.image_label.size(), aspectRatioMode=1)
         self.image_label.setPixmap(scaled_pixmap)
 
-    def show_next_image(self):
+
+    def show_next_image(self)->None:
+        """
+        display next image in main window
+        :return: None
+        """
         if self.image_iterator:
             try:
                 self.current_image = next(self.image_iterator)
@@ -70,7 +91,12 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.err_text.setText("No more images.")
                 self.err_text.show()
 
-def application():
+
+def application()->None:
+    """
+    make window
+    :return: None
+    """
     app = QtWidgets.QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
